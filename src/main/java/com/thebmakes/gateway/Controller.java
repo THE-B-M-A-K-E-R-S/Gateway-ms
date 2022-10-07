468ff92b9eb0d9150bd09dfdacc4670e2e1c2e35
Zgon <karim.mannai7@gmail.com>
2022-10-07 15:24:46 +0100

    health check

--- src/main/java/com/thebmakes/gateway/Controller.java
+++ src/main/java/com/thebmakes/gateway/Controller.java
@@ -1,12 +1,23 @@
 package com.thebmakes.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 @CrossOrigin("*")
 public class Controller {
-    @GetMapping("/test")
-    public String test() {return "wrokk";}
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/health")
+    public HashMap<String,String> health() {
+        return new HashMap<String,String>() {{
+            put("message", "Gateway is up and running on port: " + environment.getProperty("local.server.port") +" ☕🍵");
+        }};
+    }
 }
