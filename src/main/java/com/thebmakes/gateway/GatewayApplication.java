034abc5d15e1d3dfd32c36e0bab32b6a5f5d787a
BASSEM <bassem.jadoui@esprit.tn>
2022-10-20 11:47:37 +0100

    few changes ✅

--- src/main/java/com/thebmakes/gateway/GatewayApplication.java
+++ src/main/java/com/thebmakes/gateway/GatewayApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GatewayApplication {
+public class    GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
